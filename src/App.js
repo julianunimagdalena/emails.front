86753508ee885efe66231dc67893618416c39e8e
Julian <julianpitre1997@gmail.com>
2019-09-28 15:10:09 -0500

    monda

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends React.Component {
         this.state = {
             // authUser: null
             authUser: {
-                nombre: 'JULIAN PITRE',
+                nombre: 'JULIAN PITRE POLO',
                 email: 'julian@miemail.com'
             }
         };
